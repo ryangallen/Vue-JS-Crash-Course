054f8bd419fa3312529f7c8284c4615b32e43d68
Ryan Allen <allenryan14@gmail.com>
2020-10-13 02:01:42 -0400

    move header to its own component

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,45 @@
 <template>
-  <header>
-    <h1>Jot</h1>
-    <div>Write Something Down</div>
-  </header>
+  <Header />
   <Jots v-bind:user="user" />
 </template>
 
 <script>
-import Jots from './components/Jots'
+  import Header from './components/layout/Header'
+  import Jots from './components/Jots'
 
-export default {
-  name: 'App',
-  components: {
-    Jots
-  },
-  data() {
-    return {
-      user: {
-        name: "Ryan"
+  export default {
+    name: 'App',
+    components: {
+      Header,
+      Jots
+    },
+    data() {
+      return {
+        user: {
+          name: "Ryan"
+        }
       }
     }
   }
-}
 </script>
 
 <style>
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-}
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
 
-body {
-  line-height: 1.45;
-}
+  body {
+    line-height: 1.45;
+  }
 
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-header {
-  border-bottom: 5px solid #a0b8d1;
-}
-header>h1{
-  text-transform: uppercase;
-}
-header>div{
-  color: rgba(44, 62, 80, 0.50)
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <h1>Jot</h1>
+    <div>Write Something Down</div>
+  </header>
+</template>
+
+<script>
+  export default {
+    name: "Header"
+  }
+</script>
+
+<style scoped>
+  header {
+    border-bottom: 5px solid #a0b8d1;
+  }
+
+  header>h1 {
+    text-transform: uppercase;
+  }
+
+  header>div {
+    color: rgba(44, 62, 80, 0.50);
+    margin-bottom: 1px;
+  }
+</style>
